ee445ffe5ac2b2b1d1b35fb1b512d3cb181feb85
Lawrence McCary <98392880+lmccary11@users.noreply.github.com>
2022-07-24 01:30:46 -0400

    Boolean Logic

---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is you age?");
+            string ageResponse = Console.ReadLine();
+            Console.WriteLine("Have you ever had a DUI? True or False");
+            string duiResponse = Console.ReadLine();
+            Console.WriteLine("How many speeding tickets do you have?");
+            string ticketsResponse = Console.ReadLine();
+            int age = Convert.ToInt32(ageResponse);
+            bool dui = Convert.ToBoolean(duiResponse);
+            int tickets = Convert.ToInt32(ticketsResponse);
+            bool ageLimit = age > 15;
+            bool ticketLimit = tickets <= 3;
+            bool qual = !dui && ageLimit && ticketLimit;
+            Console.WriteLine("Qualified?\n" + Convert.ToString(qual));
+            Console.ReadLine();
+        }
+    }
+}
