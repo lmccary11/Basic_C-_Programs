df11b76e3bff49be87022893c8092ecbfd8b42ef
Lawrence McCary <98392880+lmccary11@users.noreply.github.com>
2022-08-05 23:22:27 -0400

    Branching Assignment

--- Branching/Branching/Program.cs
+++ Branching/Branching/Program.cs
@@ -9,18 +9,38 @@ namespace Branching
         {
             Console.WriteLine("Welcome to Package Express. Please follow the instructions below. \nPlease enter the package weight.");
             string weightEntry = Console.ReadLine();
-            Console.WriteLine("Please enter the package width.");
-            string widthEntry = Console.ReadLine();
-            Console.WriteLine("Please enter the package height.");
-            string heightEntry = Console.ReadLine();
-            Console.WriteLine("Please enter the package length.");
-            string lengthEntry = Console.ReadLine();
             int weight = Convert.ToInt32(weightEntry);
-            int width = Convert.ToInt32(widthEntry);
-            int height = Convert.ToInt32(heightEntry);
-            int length = Convert.ToInt32(lengthEntry);
-            bool tooBig = weight > 50 || height > 50 || length > 50;
-            Console.WriteLine("Your estimated total for shipping this package is $" + total)
+            bool tooHeavy = weight > 50;
+            if (tooHeavy)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Please enter the package width.");
+                string widthEntry = Console.ReadLine();
+                Console.WriteLine("Please enter the package height.");
+                string heightEntry = Console.ReadLine();
+                Console.WriteLine("Please enter the package length.");
+                string lengthEntry = Console.ReadLine();
+                int width = Convert.ToInt32(widthEntry);
+                int height = Convert.ToInt32(heightEntry);
+                int length = Convert.ToInt32(lengthEntry);
+                bool tooBig = width + height + length > 50;
+                int totalCalc = (width * height * length * weight) / 100;
+                string total = Convert.ToString(totalCalc);
+                if (tooBig)
+                {
+                    Console.WriteLine("Package too Big to be shipped via Package Express. Have a good day.");
+                    Console.ReadLine();
+                }
+                else 
+                {
+                    Console.WriteLine("Your estimated total for shipping this package is $" + total + ".00\nThanks");
+                    Console.ReadLine();
+                }
+            }
         }
     }
 }
