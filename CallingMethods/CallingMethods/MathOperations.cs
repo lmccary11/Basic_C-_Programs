46e0c243d1207ca1a7aa24c1874ddfaffd0bf5ee
Lawrence McCary <98392880+lmccary11@users.noreply.github.com>
2022-08-12 19:26:40 -0400

    CallingMethods

---
+++ CallingMethods/CallingMethods/MathOperations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class MathOperations
+    {
+        public static int method1(int x)
+        {
+            return x * x * x;
+        }
+        public static int method2(int x)
+        {
+            return x + x + x;
+        }
+        public static int method3(int x)
+        {
+            return x * x - x / x  ;
+        }
+
+        
+    }
+}
---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What number do you want the operations on?");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int calc1 = MathOperations.method1(num);
+            Console.WriteLine(calc1);
+            Console.ReadLine();
+            int calc2 = MathOperations.method2(num);
+            Console.WriteLine(calc2);
+            Console.ReadLine();
+            int calc3 = MathOperations.method3(num);
+            Console.WriteLine(calc3);
+            Console.ReadLine();
+        }
+    }
+}
