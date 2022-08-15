21501424bd8f63e74d756de9a47324f756b711f7
Lawrence McCary <98392880+lmccary11@users.noreply.github.com>
2022-08-15 16:46:13 -0400

    MethodsAndObjects

---
+++ MethodsAndObjects/MethodsAndObjects/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            emp.FirstName = "Sample";
+            emp.LastName = "Student";
+            emp.SayName();
+
+        }
+    }
+}
