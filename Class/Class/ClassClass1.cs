e487ff06585a2298302404ff02a6f1089827df01
Lawrence McCary <98392880+lmccary11@users.noreply.github.com>
2022-08-15 15:57:50 -0400

    Class

---
+++ Class/Class/ClassClass1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    public static class ClassClass1
+    {
+        public static void Method1(int x)
+        {
+            int v = x / 2;
+            Console.WriteLine(v);
+            Console.ReadLine();
+        }
+        public static int Method2(int x, out int divi)
+        {
+            divi = 0;            
+            for (int i = 1; i < 4; i++)
+            {
+                int v = x / i;
+                Console.WriteLine(v + " = " + x + " / " + i);
+                Console.ReadLine();
+                divi = i;
+                Console.WriteLine("divi = " + divi);
+                Console.ReadLine();
+            }
+            return divi;            
+        }
+        public static int Method2(string x, out int divi)
+        {
+            divi = 0;
+            int xI = Convert.ToInt32(x)*2;
+            for (int i = 1; i < 4; i++)
+            {
+                int v = xI / i;
+                Console.WriteLine(v + " = " + x + " / " + i*.5);
+                Console.ReadLine();
+                divi = i;
+                Console.WriteLine("divi = " + divi);
+                Console.ReadLine();
+            }
+            return divi;
+        }
+    }
+}
---
+++ Class/Class/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What number do you want the operations on?");
+            int num = Convert.ToInt32(Console.ReadLine());
+            ClassClass1.Method1(x: num);
+
+            ClassClass1.Method2(x: num, out int divi);
+            Console.WriteLine(divi);            
+            string divStr = Convert.ToString(divi);
+            ClassClass1.Method2(x: divStr, out int xI);
+            Console.WriteLine("xI * divi * v = " + xI + " * " + divi + " = " + xI * divi);
+            Console.ReadLine();
+
+        }
+    }
+}
