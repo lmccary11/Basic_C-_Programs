13f77b6f501125fa54737dff250728728493f44c
Lawrence McCary <98392880+lmccary11@users.noreply.github.com>
2022-08-15 13:02:27 -0400

    TwoIntMethod

---
+++ TwoIntMethod/TwoIntMethod/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoIntMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What number do you want the operations on?");
+            string num = Console.ReadLine();
+            Console.WriteLine("What other number do you want in the equation?");
+            string num2 = Console.ReadLine();
+            if (num2 == "")
+            {
+                int calc1 = TwoIntClass.TwoIntMethod1(num);
+                Console.WriteLine(calc1);
+                Console.ReadLine();
+            }
+            else
+            {
+                int calc1 = TwoIntClass.TwoIntMethod1(num, num2);
+                Console.WriteLine(calc1);
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
---
+++ TwoIntMethod/TwoIntMethod/TwoIntClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoIntMethod
+{
+    class TwoIntClass
+    {
+        public static int TwoIntMethod1(string v, string w = "4")
+        {
+            int x = Convert.ToInt32(v);
+            int u = Convert.ToInt32(w);
+            int y = x * x * x / u;
+            Console.WriteLine(x + " * " + x + " * " + x + " / " + w);
+            Console.ReadLine();
+            return y;
+        }
+    }
+}
