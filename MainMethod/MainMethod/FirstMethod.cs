acd1a7a81c4ac686103205433e5781e5d3e1e4ec
Lawrence McCary <98392880+lmccary11@users.noreply.github.com>
2022-08-15 12:09:43 -0400

    MainMethod

---
+++ MainMethod/MainMethod/FirstMethod.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class FirstMethod
+    {
+        public static int method1(int x)
+        {
+            int y = x * x * x;
+            Console.WriteLine(x + " * " + x + " * " + x);
+            Console.ReadLine();
+            return y;
+        }
+        public static int method1(decimal x)
+        {
+            int xInt = Convert.ToInt32(x);
+            int y = xInt + xInt * xInt;
+            Console.WriteLine(xInt + " + " + xInt + " * " + xInt);
+            Console.ReadLine();            
+            return y;
+        }
+        public static int method1(string x)
+        {
+            int xInt = Convert.ToInt32(x);
+            int y = xInt + xInt + xInt;
+            Console.WriteLine(xInt + " + " + xInt + " + " + xInt);
+            Console.ReadLine();
+            return y;
+        }
+    }
+
+}
---
+++ MainMethod/MainMethod/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What number do you want the operations on?");
+            string num = Console.ReadLine();
+            int calc1 = FirstMethod.method1(Convert.ToInt32(num));
+            Console.WriteLine(calc1);
+            Console.ReadLine();
+            int calc2 = FirstMethod.method1(Convert.ToDecimal(num));
+            Console.WriteLine(calc2);
+            Console.ReadLine();
+            int calc3 = FirstMethod.method1(num);
+            Console.WriteLine(calc3);
+            Console.ReadLine();
+        }
+    }
+}
