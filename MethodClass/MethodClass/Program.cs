3103c42a1edc515324e131711760f88c17826b32
Lawrence McCary <98392880+lmccary11@users.noreply.github.com>
2022-08-15 13:22:52 -0400

    MethodClass

--- MethodClass/MethodClass/Program.cs
+++ MethodClass/MethodClass/Program.cs
@@ -12,7 +12,7 @@ namespace MethodClass
         {
             Console.WriteLine("What number do you want the operations on?");
             int num = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("What other number do you want in the equation?\n This is optional.");
+            Console.WriteLine("What other number do you want in the equation?");
             int num2 = Convert.ToInt32(Console.ReadLine());
 
             MethodClass1.Method1(x: num, w: num2);
