b16f6441d8c87349fcce3e00f05e7692d600ffb2
Lawrence McCary <98392880+lmccary11@users.noreply.github.com>
2022-08-16 10:47:08 -0400

    Operators

---
+++ Operators/Operators/Employee.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Id { get; set; }
+
+        public override bool Equals(Object obj)
+        {
+            if (obj == null)
+                return false;
+
+            Employee personObj = obj as Employee;
+            if (personObj == null)
+                return false;
+            else
+                return Equals(personObj);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Id.GetHashCode();
+        }
+
+
+        public static bool operator ==(Employee person1, Employee person2)
+        {
+            return (person1.Id == person2.Id);
+        }
+
+        public static bool operator !=(Employee person1, Employee person2)
+        {
+            return (person1.Id != person2.Id);
+        }
+
+
+
+
+    }
+}
---
+++ Operators/Operators/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee joe = new Employee();
+            joe.Id = 5;
+            Employee Tim = new Employee();
+            Tim.Id = 5;
+            bool ha = joe == Tim;
+            Console.WriteLine(Convert.ToString(ha));
+            Console.ReadLine();
+
+
+        }
+    }
+}
