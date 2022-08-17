71d76008c631263ca11809d939d938c6e557a9e7
Lawrence McCary <98392880+lmccary11@users.noreply.github.com>
2022-08-17 13:29:54 -0400

    Constructor

---
+++ Constructor/Constructor/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    public class Class1
+    {
+        public Class1() : this("Softwood")
+        {
+
+        }
+        public  Class1(string paper) : this(paper, 5)
+        {
+
+        }
+        public Class1(string paper, int numMills)
+        {
+            bool good = paper == "Softwood";
+            string material = "recycled " + paper;
+            numMills = 13;      
+        }
+    }
+}
---
+++ Constructor/Constructor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var j = new int();
+            j = 10;
+            
+        }
+    }
+}
