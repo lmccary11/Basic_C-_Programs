389a603e4548c622366e2b469629bcbb772c0323
Lawrence McCary <98392880+lmccary11@users.noreply.github.com>
2022-08-17 10:31:13 -0400

    Date_and_Time

---
+++ Date and Time/Date and Time/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Date_and_Time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.ReadLine();
+            Console.WriteLine("Pick a number");
+            int x = Convert.ToInt32(Console.ReadLine());
+            TimeSpan y = new TimeSpan(x, 0, 0);
+            DateTime plusY = new DateTime();
+            plusY = DateTime.Now + y;
+
+            Console.WriteLine("It will be " + plusY + " in " + x + " hours.");
+            Console.ReadLine();
+        }
+    }
+}
