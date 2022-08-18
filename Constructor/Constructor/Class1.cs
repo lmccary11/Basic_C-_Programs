5e3d08aa618670288e9f382e9bbe1e45f4ba8631
Lawrence McCary <98392880+lmccary11@users.noreply.github.com>
2022-08-17 20:31:51 -0400

    Constructor

--- Constructor/Constructor/Class1.cs
+++ Constructor/Constructor/Class1.cs
@@ -8,26 +8,40 @@ namespace Constructor
 {
     public class Class1
     {
-        const string okayPaper = "Softwood";    //Declaring the constant okayPaper with value of softwood
-        const int NewAge = 25;                  //Declaring the constant okayPaper with value of softwood
-        public bool recycled;                   //Initial values for boolean attributes
+        private string paper;    //Initial values                          
+        public bool recycled;                   
         public bool chemicallyPulped;
 
-        public Class1() : this(okayPaper, false, true, 5)       //Constructor chained to other constructor, using constant to choose last parameter.
+        public Class1() : this("Hardwood", false, true)       
         {
+            Console.WriteLine((paper) + ((paper == "Hardwood" ^ paper == "Softwood") ? " Trees!" : " recycled sources."));
+            Console.Read();
+            return;
+        }
+        public Class1(string paper, bool recycled, bool chemicallyPulped) 
+        {
+            this.paper = paper;
+            this.recycled = recycled;
+            this.chemicallyPulped = !chemicallyPulped;
+            Console.WriteLine((paper) + ((paper == "Hardwood" ^ paper == "Softwood") ? " Trees!" : " recycled sources."));
+            Console.Read();
+            return;
+        }
+        public Class1(string paper) : this(paper, false, true)       
+        {
+            //Determining that good operations are when wood is either chemically pulped or recycled.
+            //Testing that the good state is true in the present and if so which process is occurring.
+            //Description of how the material is prepared.
+            Console.WriteLine(paper + ((paper == "Hardwood" ^ paper == "Softwood") ? " Trees!" : " recycled sources."));
+            Console.Read();
+            return;
 
         }
-        public Class1(string paper) : this(paper, true, false, 3) //Constructor initializing 3 things on the Class1 object.
+        public Class1(bool recycled) : this("Newspaper", recycled, true)      
         {
-        }
-        public Class1(string paper, bool recycled, bool chemicallyPulped, int numMills) //Object with parameters.
-        {            
-            bool good = paper == "Softwood" && (chemicallyPulped == true ^ recycled == true);  
-            //Determining that good operations are when Softwood is either chemically pulped or recycled, not neither, not both, no hardwood.         
-            var process = good ? ((chemicallyPulped == !recycled) ? "chemically pulped chips from " : "mechanically pulped recycled bales of ") : "Not a viable process for ";
-            //Testing that the good state is true in the present and if so which process is occurring.
-            var material = process + paper;                 //Description of how the material is prepared.
-            var marketLongevity = Class1.NewAge * numMills; //Calculation of years and depth of mills operational.
+            Console.WriteLine((paper) + ((paper == "Hardwood" ^ paper == "Softwood") ? " Trees!" : " recycled sources."));
+            Console.Read();
+
             return;
         }
     }
--- Constructor/Constructor/Program.cs
+++ Constructor/Constructor/Program.cs
@@ -8,9 +8,32 @@ namespace Constructor
 {
     class Program
     {
+        const string okayPaper = "Softwood";
         
+
         static void Main(string[] args)
         {
+            Class1 classOne = new Class1(okayPaper, true, false);
+            var one = Console.ReadLine().Length;
+            Class1 classTwo = new Class1();
+            var two = Console.ReadLine().Length;
+            Class1 classThree = new Class1("Newspaper");
+            var three = Console.ReadLine().Length;
+            Class1 classFour = new Class1(true);
+            var four = Console.ReadLine().Length;
+
+            
+
         }
+
+
     }
+        
+            
+       
+       
 }
+        
+                          
+            
+ 
